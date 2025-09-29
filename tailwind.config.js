effbe1bd4750248051f96f0581260a286fc87618
AtharvKhunte <144105070+AtharvKhunte@users.noreply.github.com>
2025-09-29 16:33:30 +0530

    Updated tailwind.config.js to enable dark-mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx}", "./public/index.html"],
+  darkMode: "class", // enable class-based dark mode
   theme: {
     extend: {},
   },
